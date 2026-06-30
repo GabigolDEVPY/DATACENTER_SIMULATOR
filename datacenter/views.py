7bfb4085657f4026046b267f9b7ed5a10e9422d0
Gabriel Rocha Dias <gabigolbr36@gmail.com>
2026-06-30 15:32:15 -0300

    arrumando o django

--- datacenter/views.py
+++ datacenter/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.views.generic import View
+from .models import DataCenterHack
 
 class HomeView(View):
     def get(self, request):
+        
+        def get_context_data(self, **kwargs):
+            context = super().get_context_data(**kwargs)
+            context["hacks"] = DataCenterHack.objects.all()
+            return context
+    
+    
         return render(request, 'datacenter/index.html')
