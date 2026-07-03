3097f97ae5fe21767919cd963f74021f76038361
Gabriel Rocha Dias <gabigolbr36@gmail.com>
2026-07-03 19:35:35 -0300

    arrumando arquivos

--- datacenter/urls.py
+++ datacenter/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
-from .views import HomeView, GetBayDetail
+from .views.views import HomeView, GetBayDetail
+from .views.bays_views import ChangeStatusBay
 
 app_name = "datacenter"
 
 urlpatterns = [
     path('', HomeView.as_view(), name="home"),
-    path('get-bay-detail/<int:id>', GetBayDetail.as_view(), name="get_bay_detail")
+    
+    
+    # bays
+    path('get-bay-detail/<int:id>', GetBayDetail.as_view(), name="get_bay_detail"),
+    path('bay-status-changed/<int:id>', ChangeStatusBay.as_view(), name="change_status_bay")
 ]
 
 
---
+++ datacenter/views/bays_views.py
@@ -0,0 +1,13 @@
+import sys
+sys.dont_write_bytecode = True
+from django.shortcuts import render
+from django.views.generic import View, TemplateView
+from ..models import DataCenterHack, Bay
+
+
+class ChangeStatusBay(View):
+    def post(self, request, id):
+        bay = Bay.objects.filter(id=id).first()
+        bay.is_active = not bay.is_active
+        bay.save()
+        return render(request, template_name="partials/bay.html", context={"bay": bay})
