96ff6602e406ce28305ff4c3c2a2ac238b1a3820
Gabriel Rocha Dias <gabigolbr36@gmail.com>
2026-08-22 15:36:41 -0300

    fdasf

--- server/consumers.py
+++ server/consumers.py
@@ -3,20 +3,24 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 
 class ServerConsumer(AsyncWebsocketConsumer):
     async def connect(self):
+        try:
+            self.group_name = f"user_{self.scope['user'].id}"
+            await self.channel_layer.group_add(
+                self.group_name,
+                self.channel_name
+            )
+            await self.accept()
+        except Exception as e:
+            await self.close(code=1011) # erro interno do servidor
         
-        self.group_name = f"user_{self.scope['user'].id}"
+             
         
-        await self.channel_layer.group_add(
-            self.group_name,
-            self.channel_name
-        )
-        await self.accept()
-    
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(
             self.group_name,
             self.channel_name
         )
     
+    # serviço é chamado quando existe updates dos dados do usuário
     async def power_update(self, event):
         await self.send(text_data=json.dumps(event))
