15bca2dbfd01dd5a6cfa0a9774a33bcb0be611de
Gabriel Rocha Dias <gabigolbr36@gmail.com>
2026-08-23 13:30:56 -0300

    arrumando raridades

--- server/models.py
+++ server/models.py
@@ -38,19 +38,25 @@ class Bay(models.Model):
         WattsTier.VERY_HIGH: 280000,
     }
 
-    gpu = models.ForeignKey(InventoryItem, on_delete=models.CASCADE, null=True, blank=True, related_name="gpu_bay")
-    ssd = models.ForeignKey(InventoryItem, on_delete=models.CASCADE, null=True, blank=True, related_name="ssd_bay")
     cpu = models.ForeignKey(InventoryItem, on_delete=models.CASCADE, null=True, blank=True, related_name="cpu_bay")
-    ram = models.ForeignKey(InventoryItem, on_delete=models.CASCADE, null=True, blank=True, related_name="ram_bay")
+    ssd = models.ForeignKey(InventoryItem, on_delete=models.CASCADE, null=True, blank=True, related_name="ssd_bay")
     
+    gpu1 = models.ForeignKey(InventoryItem, on_delete=models.CASCADE, null=True, blank=True, related_name="gpu_bay1")
+    gpu2 = models.ForeignKey(InventoryItem, on_delete=models.CASCADE, null=True, blank=True, related_name="gpu_bay2")
+    gpu3 = models.ForeignKey(InventoryItem, on_delete=models.CASCADE, null=True, blank=True, related_name="gpu_bay3")
+    
+    ram1 = models.ForeignKey(InventoryItem, on_delete=models.CASCADE, null=True, blank=True, related_name="ram_bay1")
+    ram2 = models.ForeignKey(InventoryItem, on_delete=models.CASCADE, null=True, blank=True, related_name="ram_bay2")
+    ram3 = models.ForeignKey(InventoryItem, on_delete=models.CASCADE, null=True, blank=True, related_name="ram_bay3")
+
     @property
     def get_power(self):
-        power =(self.cpu.get_power() + self.gpu.get_power() + self.ssd.get_power() + self.ram.get_power())
+        power =(self.cpu.get_power() + self.gpu1.get_power() + self.gpu2.get_power() + self.gpu3.get_power() + self.ssd.get_power() + self.ram1.get_power() + self.ram2.get_power() + self.ram3.get_power())
         return power
 
     @property
     def get_energy_rate(self):
-        energy_rate =(self.get_cpu.get_energy_rate() + self.get_gpu.get_energy_rate() + self.get_ssd.get_energy_rate() + self.get_ram.get_energy_rate())
+        energy_rate =(self.get_cpu.get_energy_rate() + self.get_gpu1.get_energy_rate() + self.get_gpu2.get_energy_rate() + self.get_gpu3.get_energy_rate() + self.get_ssd.get_energy_rate() + self.get_ram1.get_energy_rate() + self.get_ram2.get_energy_rate() + self.get_ram3.get_energy_rate())
         return energy_rate
 
     @property
@@ -58,22 +64,40 @@ class Bay(models.Model):
         return self.WATTS_PRICE.get(self.watts, 0)
 
 
-    # get das partes do bay
-    @property
-    def get_cpu(self):
-        return self.cpu.item.cpu if self.cpu else None
-
-    @property
-    def get_gpu(self):
-        return self.gpu.item.gpu if self.gpu else None
-
-    @property
-    def get_ram(self):
-        return self.ram.item.ram if self.ram else None
-
+    # get das peças
     @property
     def get_ssd(self):
         return self.ssd.item.ssd if self.ssd else None
 
     def __str__(self):
         return f" {self.name}"
+
+    # get das partes do bay
+    @property
+    def get_cpu(self):
+        return self.cpu.item.cpu if self.cpu else None
+
+    @property
+    def get_gpu1(self):
+        return self.gpu1.item.gpu if self.gpu1 else None
+
+    @property
+    def get_gpu2(self):
+        return self.gpu2.item.gpu if self.gpu2 else None
+
+    @property
+    def get_gpu3(self):
+        return self.gpu3.item.gpu if self.gpu3 else None
+
+    @property
+    def get_ram1(self):
+        return self.ram1.item.ram if self.ram1 else None
+
+    @property
+    def get_ram2(self):
+        return self.ram2.item.ram if self.ram2 else None
+
+    @property
+    def get_ram3(self):
+        return self.ram3.item.ram if self.ram3 else None
+
