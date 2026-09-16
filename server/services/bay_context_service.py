2388dacf8015ab9781617f880feeda525c335035
Gabriel Rocha Dias <gabigolbr36@gmail.com>
2026-09-16 20:32:40 -0300

    ajustando views context

--- server/services/bay_context_service.py
+++ server/services/bay_context_service.py
@@ -2,29 +2,29 @@ from user.services.inventory_services import InventoryService
 from server.services.bay_service import BayService
 
 class BayContextService:
+    def _build_context(bay, user_id):
+        components = InventoryService(user_id).get_components()
+        return {"bay": bay, **components}
+    
     @staticmethod
     def get_bay_model_context(user_id, bay_id):
         bay = BayService(bay_id=bay_id).get_view_model()
-        components = InventoryService(user_id).get_components()
-        return bay, components
+        return BayContextService._build_context(bay, user_id)
     
     @staticmethod
     def change_component(data, user_id, bay_id):
-        components = InventoryService(user_id).get_components()
         bay = BayService(bay_id=bay_id).change_component(data)
-        return bay, components
+        return BayContextService._build_context(bay, user_id)
         
     @staticmethod
     def change_status(user_id, bay_id):
         bay = BayService(bay_id).change_status()
-        components = InventoryService(user_id).get_components()
-        return bay, components
+        return BayContextService._build_context(bay, user_id)
     
     @staticmethod
     def remove_component(data, user_id, bay_id):
         bay = BayService(bay_id).remove_component(data)
-        components = InventoryService(user_id).get_components()
-        return bay, components
+        return BayContextService._build_context(bay, user_id)
         
     
         
--- server/views/bays_views.py
+++ server/views/bays_views.py
@@ -7,26 +7,22 @@ from server.services.bay_context_service import BayContextService
 
 class ChangeStatusBay(View):
     def post(self, request, id):
-        bay, components = BayContextService.change_status(request.user.id, id)
-        context = {"bay": bay, **components}
+        context = BayContextService.change_status(request.user.id, id)
         return render(request, template_name="partials/bay_status_response.html", context=context)
     
     
 class GetBayDetail(View):
     def get(self, request, id):
-        bay, components = BayContextService.get_bay_model_context(request.user.id, bay_id=id)
-        context = {"bay": bay, **components}
+        context = BayContextService.get_bay_model_context(request.user.id, bay_id=id)
         return render(request, template_name="partials/modal_bay.html", context=context)
     
     
 class ChangeComponent(View):
     def post(self, request, id):
-        bay, components = BayContextService.change_component(request.POST, request.user.id, id)
-        context = {"bay": bay, **components}
+        context = BayContextService.change_component(request.POST, request.user.id, id)
         return render(request, template_name="partials/modal_bay.html", context=context)
     
 class RemoveComponent(View):
     def post(self, request, id):
-        bay, components = BayContextService.remove_component(request.POST, request.user.id, id)
-        context = {"bay": bay, **components}
+        context = BayContextService.remove_component(request.POST, request.user.id, id)
         return render(request, template_name="partials/modal_bay.html", context=context)
