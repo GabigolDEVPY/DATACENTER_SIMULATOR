aeee8e9e5a4f5a89fc13fd5a237cbdb30e5359ba
Gabriel Rocha Dias <gabigolbr36@gmail.com>
2026-09-20 20:39:07 -0300

    arrumando algumas coisas

--- model/urls.py
+++ model/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from .views import HomeView, IAModelDetailView
+from .views import HomeView, IAModelDetailView, IaModelRun
 
 app_name = "model"
 
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
     
-    path("ia-detail/<int:id>", IAModelDetailView.as_view(), name="get_ia_detail")
+    path("ia-detail/<int:id>", IAModelDetailView.as_view(), name="get_ia_detail"),
+    path("ia_model_run/<int:id>", IaModelRun.as_view(), name="ia_model_run")
     
 ]
 
--- model/views.py
+++ model/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import View, TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from model.services.ia_model_context_service import IaModelContextServices
+from django.http import JsonResponse
 
 # Create your views here.
 class HomeView(LoginRequiredMixin, TemplateView):
@@ -18,3 +19,12 @@ class IAModelDetailView(LoginRequiredMixin, View):
     def get(self, request, id):
         context = IaModelContextServices.get_ia_model(self.request.user.id, id)
         return render(request, template_name="partials/ia_modal.html", context=context)
+    
+class IaModelRun(LoginRequiredMixin, View):
+    def post(self, request, id):
+        print(request.POST)
+        return JsonResponse({
+            "fdsfd": 200
+        })
+        
+        
